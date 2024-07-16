64684879438e858e402118d9f4415a498a65cae0
Preston Chapman <pchapman82070@uat.edu>
2024-07-16 10:57:36 -0700

    Add Functions

--- script/main.js
+++ script/main.js
@@ -9,5 +9,31 @@ function setUp(){
         hole.classList.add("hole");
         hole.setAttribute("id", `hole-${i}`);
         board.appendChild(hole);
+        hole.addEventListener("mouseover", (e) => startHover(e));
+        hole.addEventListener("click", (e) => onClick(e));
+        hole.addEventListener("mouseleave", (e)=> stopHover(e))
     }
+}
+
+/**
+ * 
+ * @param {MouseEvent} event 
+ */
+function startHover(event){
+    console.log("Hovering");
+}
+
+/**
+ * 
+ * @param {MouseEvent} event 
+ */
+function onClick(event){
+    console.log("Clicked")
+}
+/**
+ * 
+ * @param {MouseEvent} event 
+ */
+function stopHover(event){
+    console.log("Stopped Hovering")
 }
