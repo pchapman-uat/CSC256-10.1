3b7d9d442d82c94903b6a2100b74dde0e78e6f0a
Preston Chapman <pchapman82070@uat.edu>
2024-07-16 10:48:41 -0700

    Generate Holes

--- script/main.js
+++ script/main.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+document.addEventListener("DOMContentLoaded", () => setUp());
+
+const holes = 6;
+
+function setUp(){
+    var board = document.getElementById("board");
+    for(let i=0; i<holes;i++){
+        let hole = document.createElement("div");
+        hole.classList.add("hole");
+        hole.setAttribute("id", `hole-${i}`);
+        board.appendChild(hole);
+    }
+}
